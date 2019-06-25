bb48970313950dcef054f9172d7bd42b25a4d76a
Suman Kumar Hansada <Suman@ash-software.com>
2019-06-25 12:43:20 +0530

    Added Automapper and WebAPI for Customer

---
+++ Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<Customer, CustomerDto>();
+                cfg.CreateMap<CustomerDto, Customer>();
+            });
+            config.CreateMapper();
+
+        }
+    }
+}
