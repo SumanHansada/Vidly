d7925df25f66604995b7b78869f1b00b05f75855
Suman Kumar Hansada <Suman@ash-software.com>
2019-07-08 13:09:50 +0530

    Added Memory Cache

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -1,8 +1,10 @@
-﻿using System.Linq;
+﻿using System.Runtime.Caching;
+using System.Linq;
 using System.Web.Mvc;
 using Vidly.Models;
 using System.Data.Entity;
 using Vidly.ViewModels;
+using System.Collections.Generic;
 
 namespace Vidly.Controllers
 {
@@ -61,6 +63,13 @@ namespace Vidly.Controllers
         // GET: Customers
         public ViewResult Index()
         {
+            if(MemoryCache.Default["Genres"] == null)
+            {
+                MemoryCache.Default["Genres"] = _context.Genres.ToList();
+            }
+
+            var genres = MemoryCache.Default["Genres"] as IEnumerable<Genre>;
+
             return View();
         }
 
